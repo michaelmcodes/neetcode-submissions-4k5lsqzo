475bf2bc9ee034fbc3ba0193091824c290b3f110
Michael Mark <michaelmarkcs@gmail.com>
2026-04-10 14:23:34 -0400

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> dup = new HashSet<>();
+        for(int n : nums){
+            if(dup.contains(n)){
+                return true;
+            }
+            dup.add(n);
+        }
+        return false;
+    }
+}
