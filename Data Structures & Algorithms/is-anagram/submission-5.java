ec75745257dd74912958ff22f64596d1fa89f610
Michael Mark <michaelmarkcs@gmail.com>
2026-04-10 16:01:36 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26]; 
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        for(int i=0; i<s.length(); i++){
+            count[sArray[i] - 'a'] += 1;
+            count[tArray[i] - 'a'] -= 1;
+        }
+        for(int num : count){
+            if(num != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
