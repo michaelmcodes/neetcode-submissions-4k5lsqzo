9a505c83fc9366f22904cbb06a915197422528a5
Michael Woke <michaelmarkcs@gmail.com>
2026-04-27 12:17:14 -0400

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> dup = new HashSet<>();
+
+        for(int num : nums){
+            if(dup.contains(num)){
+                return true;
+            }
+            dup.add(num);
+        }
+        return false;
+    }
+}
