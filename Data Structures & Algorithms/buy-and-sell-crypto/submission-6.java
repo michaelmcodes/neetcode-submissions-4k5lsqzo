5b191d7821e450f545dffc1bb75d7c0b1f6e3bbb
Michael Woke <michaelmarkcs@gmail.com>
2026-04-28 11:09:41 -0400

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0, r = 1;
+        int profit = 0;
+        int max = 0;
+
+        while(r < prices.length){
+            if(prices[r] > prices[l]){
+                profit = prices[r] - prices[l];
+                max = Math.max(profit, max);
+            }
+            else{
+                l = r;
+            }
+            r++;
+
+        }
+        return max;
+    }
+}
