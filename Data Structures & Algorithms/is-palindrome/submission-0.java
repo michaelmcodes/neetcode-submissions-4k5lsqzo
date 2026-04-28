a818d1bf14f224c547ebfd626fbd704346b7ec4b
Michael Woke <michaelmarkcs@gmail.com>
2026-04-28 14:48:57 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String clean = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int l = 0;
+        int r = clean.length() - 1;
+
+        while(r > l){
+            if(clean.charAt(l) != clean.charAt(r)) {return false; }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
